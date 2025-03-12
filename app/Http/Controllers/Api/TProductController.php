a627fdab18025898def4869b8462e16c79dd0b1b
HFM007 <habib.fauz43@gmail.com>
2025-03-12 13:38:16 +0700

    Add TProductController for managing TProduct resources

---
+++ app/Http/Controllers/Api/TProductController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\TProduct;
+use Illuminate\Http\Request;
+
+class TProductController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    $data = TProduct::all();
+    return response()->json([
+      'status' => 1,
+      'data' => $data,
+      'message' => 'Data ditemukan!'
+    ], 200);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(string $id)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(string $id)
+  {
+    //
+  }
+}
