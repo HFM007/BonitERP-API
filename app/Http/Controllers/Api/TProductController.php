72dfe7e0ceb1419d82c60b64bbef0c9abc43d964
HFM007 <habib.fauz43@gmail.com>
2025-03-12 14:50:37 +0700

    Enhance update and destroy methods in TProductController with validation and improved response handling

--- app/Http/Controllers/Api/TProductController.php
+++ app/Http/Controllers/Api/TProductController.php
@@ -59,6 +59,8 @@ class TProductController extends Controller
     $post = $data->save();
 
     if ($post) {
+      $data->makeHidden('t_products_id');
+
       return response()->json([
         'status' => '1',
         'data' => $data,
@@ -81,6 +83,8 @@ class TProductController extends Controller
     $data = TProduct::where('t_products_id', $id)->first();
 
     if ($data) {
+      $data->makeHidden('t_products_id');
+
       return response()->json([
         'status' => 1,
         'data' => $data,
@@ -101,14 +105,108 @@ class TProductController extends Controller
    */
   public function update(Request $request, string $id)
   {
-    //
+    $data = TProduct::where('t_products_id', $id)->first();
+
+    if (empty($data)) {
+      return response()->json([
+        'status' => 0,
+        'message' => 'Data tidak ditemukan!'
+      ], 404);
+    }
+
+    $rules = [
+      't_products_nama' => 'required',
+      't_products_harga' => 'required',
+      't_products_deskripsi' => 'required',
+      't_products_stok' => 'required',
+      't_products_kategori' => 'required',
+      't_products_gambar' => 'required',
+      't_products_status' => 'required',
+    ];
+
+    $validator = Validator::make($request->all(), $rules);
+
+    if ($validator->fails()) {
+      return response()->json([
+        'status' => 0,
+        'data' => $validator->errors(),
+        'message' => 'Data gagal diperbarui!'
+      ], 400);
+    }
+
+    $data->t_products_nama = $request->t_products_nama;
+    $data->t_products_harga = $request->t_products_harga;
+    $data->t_products_deskripsi = $request->t_products_deskripsi;
+    $data->t_products_stok = $request->t_products_stok;
+    $data->t_products_kategori = $request->t_products_kategori;
+    $data->t_products_gambar = $request->t_products_gambar;
+    $data->t_products_status = $request->t_products_status;
+
+    $post = $data->save();
+
+    if ($post) {
+      $data->makeHidden('t_products_id');
+
+      return response()->json([
+        'status' => 1,
+        'data' => $data,
+        'message' => 'Data berhasil diperbarui!'
+      ], 200);
+    } else {
+      return response()->json([
+        'status' => 0,
+        'data' => null,
+        'message' => 'Data gagal diperbarui!'
+      ], 400);
+    }
   }
 
   /**
    * Remove the specified resource from storage.
    */
-  public function destroy(string $id)
+  public function destroy(Request $request, string $id)
   {
-    //
+    $data = TProduct::where('t_products_id', $id)->first();
+
+    if (empty($data)) {
+      return response()->json([
+        'status' => 0,
+        'message' => 'Data tidak ditemukan!'
+      ], 404);
+    }
+
+    $rules = [
+      't_products_status' => 'required',
+    ];
+
+    $validator = Validator::make($request->all(), $rules);
+
+    if ($validator->fails()) {
+      return response()->json([
+        'status' => 0,
+        'data' => $validator->errors(),
+        'message' => 'Data gagal diperbarui!'
+      ], 400);
+    }
+
+    $data->t_products_status = $request->t_products_status;
+
+    $post = $data->save();
+
+    if ($post) {
+      $data->makeHidden('t_products_id', 't_products_nama', 't_products_harga', 't_products_deskripsi', 't_products_stok', 't_products_kategori', 't_products_gambar');
+
+      return response()->json([
+        'status' => 1,
+        'data' => $data,
+        'message' => 'Data berhasil diperbarui!'
+      ], 200);
+    } else {
+      return response()->json([
+        'status' => 0,
+        'data' => null,
+        'message' => 'Data gagal diperbarui!'
+      ], 400);
+    }
   }
 }
