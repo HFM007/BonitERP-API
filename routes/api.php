afb9de155d2d113afae39f8cf38dc9d65ffa369f
HFM007 <habib.fauz43@gmail.com>
2025-03-12 14:58:28 +0700

    Refactor product routes to use a route group for better organization

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,10 @@ Route::get('/user', function (Request $request) {
   return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::get('/product', [TProductController::class, 'index']);
-Route::get('/product/{id}', [TProductController::class, 'show']);
-Route::post('/product', [TProductController::class, 'store']);
-Route::put('/product/{id}', [TProductController::class, 'update']);
-Route::put('/product/{id}', [TProductController::class, 'destroy']);
+Route::prefix('product')->group(function () {
+  Route::get('/', [TProductController::class, 'index']);
+  Route::get('/{id}', [TProductController::class, 'show']);
+  Route::post('/', [TProductController::class, 'store']);
+  Route::put('/{id}', [TProductController::class, 'update']);
+  Route::put('/{id}', [TProductController::class, 'destroy']);
+});
