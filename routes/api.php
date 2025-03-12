c707d3fc39b56b6586c370ff16c64f0b3892387d
HFM007 <habib.fauz43@gmail.com>
2025-03-12 14:50:52 +0700

    Fix route for TProductController destroy method to use PUT instead of DELETE

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,4 @@ Route::get('/product', [TProductController::class, 'index']);
 Route::get('/product/{id}', [TProductController::class, 'show']);
 Route::post('/product', [TProductController::class, 'store']);
 Route::put('/product/{id}', [TProductController::class, 'update']);
-Route::delete('/product/{id}', [TProductController::class, 'destroy']);
+Route::put('/product/{id}', [TProductController::class, 'destroy']);
