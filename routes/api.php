e86f06a327cd89a043a4a3a3955a92b24897f0e4
HFM007 <habib.fauz43@gmail.com>
2025-03-12 13:44:52 +0700

    Add CRUD routes for product management in API

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 })->middleware('auth:sanctum');
 
 
 Route::get('/product', [TProductController::class, 'index']);
+Route::get('/product/{id}', [TProductController::class, 'show']);
+Route::post('/product', [TProductController::class, 'store']);
+Route::put('/product/{id}', [TProductController::class, 'update']);
+Route::delete('/product/{id}', [TProductController::class, 'destroy']);
