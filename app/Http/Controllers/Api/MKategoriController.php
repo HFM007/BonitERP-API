3303fd6ce9df8dc76cfeebec5a5df3a426a04186
HFM007 <Habib.fauz43@gmail.com>
2025-03-15 19:01:54 +0700

    Replace MKategori controller with MKategoriController for improved structure and functionality

---
+++ app/Http/Controllers/Api/MKategoriController.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\MKategori;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class MKategoriController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    $data = MKategori::all();
+    return response()->json([
+      'status' => 1,
+      'data' => $data,
+      'message' => 'Data ditemukan!'
+    ], 200);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    $rules = [
+      'm_kategori_nama' => 'required',
+      'm_kategori_status' => 'required'
+    ];
+    
+    $validator = Validator::make($request->all(), $rules);
+    
+    if ($validator->fails()){
+      return response()->json([
+        'status' => 0,
+        'data' => $validator->errors(),
+        'message' => 'Data gagal ditambahkan!'
+      ]);
+    }
+    
+    $data = new MKategori();
+    $data->m_kategori_nama = $request->m_kategori_nama;
+    $data->m_kategori_status = 1;
+
+    $post = $data->save();
+
+    if ($post) {
+      return response()->json([
+        'status' => 1,
+        'data' => $data,
+        'message' => 'Data berhasil ditambahkan!'
+      ], 200);
+    } else {
+      return response()->json([
+        'status' => 0,
+        'message' => 'Data gagal ditambahkan!'
+      ], 400);
+    }
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(string $id)
+  {
+    $data = MKategori::where('m_kategori_id', $id)->first();
+    if ($data) {
+      return response()->json([
+        'status' => 1,
+        'data' => $data,
+        'message' => 'Data ditemukan!'
+      ], 200);
+    } else {
+      return response()->json([
+        'status' => 0,
+        'data' => null,
+        'message' => 'Data tidak ditemukan!'
+      ], 404);
+    }
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id)
+  {
+    $data = MKategori::where('m_kategori_id', $id)->first();
+
+    if(empty($data)) {
+      return response()->json([
+        'status' => 0,
+        'message' => 'Data tidak ditemukan!'
+      ], 404);
+    }
+
+    $rules = [
+      'm_kategori_nama' => 'required',
+      'm_kategori_status' => 'required',
+    ];
+
+    $validator = Validator::make($request->all(), $rules);
+
+    if( $validator->fails() ) {
+      return response()->json([
+        'status' => 0,
+        'data' => $validator->errors(),
+        'message' => 'Data gagal diupdate!'
+      ]);
+    }
+
+    $data = new MKategori();
+    $data->m_kategori_nama = $request->m_kategori_nama;
+    $data->m_kategori_status = $request->m_kategori_status;
+
+    $post = $data->save();
+
+    if($post){
+      return response()->json([
+        'status' => 1,
+        'data' => $data,
+        'message' => 'Data berhasil diupdate!'
+      ], 200);
+    } else{
+      return response()->json([
+        'status' => 0,
+        'message' => 'Data gagal diupdate!'
+      ], 400);
+    }
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Request $request, string $id)
+  {
+    $data = MKategori::where('m_kategori_id', $id)->first();
+
+    $rules = [
+      'm_kategori_status' => 'required',
+    ];
+
+    $validator = Validator::make($request->all(), $rules);
+    
+    if($validator->fails()){
+      return response()->json([
+        'status' => 0,
+        'data' => $validator->errors(),
+        'message' => 'Data gagal dihapus!'
+      ]);
+    }
+
+    $data->m_kategori_status = $request->m_kategori_status;
+
+    $post = $data->save();
+
+    if ($post) {
+      return response()->json([
+        'status' => 1,
+        'data' => $data,
+        'message' => 'Data berhasil dihapus!'
+      ], 200);
+    } else {
+      return response()->json([
+        'status'=> 0,
+        'message'=> 'Data gagal dihapus!'
+      ], 400);
+    }
+
+  }
+}
