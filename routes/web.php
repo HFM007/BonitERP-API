fb5cc44cfb4662b647dccc2ed1838226ec0888ea
HFM007 <Habib.fauz43@gmail.com>
2025-03-15 15:18:31 +0700

    Comment out product routes in web.php for future implementation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TProductController;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+// Route::get('/product', [TProductController::class, 'index']);
+// Route::get('/product/{id}', [TProductController::class, 'show']);
+// Route::post('/product', [TProductController::class, 'store']);
+// Route::put('/product/update/{id}', [TProductController::class, 'update']);
+// Route::put('/product/delete/{id}', [TProductController::class, 'destroy']);
