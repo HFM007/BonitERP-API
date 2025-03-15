6c810956f2e432469f9cd7c47142c2306db7a68e
HFM007 <Habib.fauz43@gmail.com>
2025-03-15 17:57:32 +0700

    Add migration for m_kategori table with columns for ID, name, and status

---
+++ database/migrations/2025_03_15_092713_create_m_kategori_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('m_kategori', function (Blueprint $table) {
+      $table->id('m_kategori_id');
+      $table->string('m_kategori_nama');
+      $table->string('m_kategori_status');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('m_kategori');
+  }
+};
