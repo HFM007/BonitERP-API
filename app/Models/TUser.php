d24fcf8816bf395ff063030c17ce0c12a9486f89
HFM007 <Habib.fauz43@gmail.com>
2025-05-05 21:36:42 +0700

    Create TUser model with fillable and hidden attributes

---
+++ app/Models/TUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class TUser extends Authenticatable
+{
+  use HasFactory, Notifiable, HasApiTokens;
+
+  protected $table = "t_user";
+  protected $primaryKey = "t_user_id";
+  public $timestamps = false;
+  protected $fillable = [
+    't_user_nama',
+    't_user_email',
+    't_user_password',
+    't_user_status',
+  ];
+
+  protected $hidden = [
+    't_user_password',
+  ];
+}
