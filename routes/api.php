a12ebb865feb4c51d5d38a2fa29b6f3042b22cef
HFM007 <Habib.fauz43@gmail.com>
2025-05-06 19:10:05 +0700

    Comment out user route for authentication

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,9 @@ use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-  return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//   return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::prefix('auth')->group(function () {
   Route::post('/register', [RegisterController::class, '__invoke']);
