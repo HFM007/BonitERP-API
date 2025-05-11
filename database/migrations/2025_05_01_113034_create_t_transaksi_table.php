623527a2913cdf5190b209d5e82239bf88b70237
HFM007 <Habib.fauz43@gmail.com>
2025-05-11 20:19:06 +0700

    Remove foreign key and quantity field from t_transaksi migration to simplify structure

--- database/migrations/2025_05_01_113034_create_t_transaksi_table.php
+++ database/migrations/2025_05_01_113034_create_t_transaksi_table.php
@@ -4,30 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('t_transaksi', function (Blueprint $table) {
-            $table->id('t_transaksi_id');
-            $table->foreignId('t_transaksi_user_id')->constrained('t_user', 't_user_id')->onDelete('cascade');
-            $table->foreignId('t_transaksi_produk_id')->constrained('t_products', 't_products_id')->onDelete('cascade');
-            $table->integer('t_transaksi_jumlah');
-            $table->string('t_transaksi_total_harga');
-            $table->string('t_transaksi_jenis_pembayaran');
-            $table->integer('t_transaksi_status');
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('t_transaksi', function (Blueprint $table) {
+      $table->id('t_transaksi_id');
+      $table->foreignId('t_transaksi_user_id')->constrained('t_user', 't_user_id')->onDelete('cascade');
+      $table->string('t_transaksi_total_harga');
+      $table->string('t_transaksi_jenis_pembayaran');
+      $table->integer('t_transaksi_status');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('t_transaksi');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('t_transaksi');
+  }
 };
